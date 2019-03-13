520336cf0c52f8cb881d083a33f5cfd8e7be289d
zzhpeng <zzhpeng@zzhpeng.lan>
2019-03-13 14:04:23 +0800

    database

---
+++ data/conf/route.php
@@ -0,0 +1,71 @@
+<?php	return array (
+  '营养健身/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '型男/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '靓妹/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=3',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '营养健身' => 
+  array (
+    0 => 'portal/List/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '型男' => 
+  array (
+    0 => 'portal/List/index?id=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '靓妹' => 
+  array (
+    0 => 'portal/List/index?id=3',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+);
